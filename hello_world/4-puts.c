d539b80a71d977c38958d60eb2473718b92e0aea
root <kebdanisajjad271199@gmail.com>
2022-06-08 12:20:28 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
- /**
+ 
+/**
  * main - Entrée
  * 
  * Return: toujours à 0 (Success)
  */
-int main(void)
+int main (void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
         return (0);
