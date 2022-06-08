0fb713588f3916fb375d4b76a90ded200627b601
root <kebdanisajjad271199@gmail.com>
2022-06-08 11:01:00 -0700

    task4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -6,4 +6,6 @@
 * Return:toujours 0 (success)
 */
 int main(void)
-puts "Programming is like building a multilingual puzzle	
+puts ("Programming is like building a multilingual puzzle")
+return (0);
+}
