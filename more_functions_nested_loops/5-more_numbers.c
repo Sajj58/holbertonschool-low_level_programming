5c056d2fc8f0a1b107190bf903574caf7380c75e
root <kebdanisajjad271199@gmail.com>
2022-06-17 14:50:24 -0700

    5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers- print 0 of 14
+ *
+ */
+
+void more_numbers(void)
+{
+	int a, n;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar((n / 10) + '0');
+			}
+			_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
