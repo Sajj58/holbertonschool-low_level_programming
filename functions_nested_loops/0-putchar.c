d1ad32affe570ae9baec66df38dac00c02035f92
root <kebdanisajjad271199@gmail.com>
2022-06-17 14:16:28 -0700

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include"main.h"
+#include <stdio.h>
+/**
+* main - Prints "_putchar" followed by a new line.
+*
+* Return: Always 0 (Succes).
+*/
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
