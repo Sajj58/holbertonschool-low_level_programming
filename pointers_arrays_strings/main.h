65dbbb9f9acaa28263238f298fdbb5647414122f
root <kebdanisajjad271199@gmail.com>
2022-06-17 15:27:32 -0700

    8

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,5 +11,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 void print_array(int *a, int n);
-void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
