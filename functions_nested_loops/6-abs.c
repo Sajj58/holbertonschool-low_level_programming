3892009e8f70e765dccee842de76506d55cdb5fb
root <kebdanisajjad271199@gmail.com>
2022-06-22 07:19:32 -0700

    ss

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Cfr description
+ * @r: input number
+ * Description: return absolute value of an integer
+ * Return: absolute value of an integer
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
+
