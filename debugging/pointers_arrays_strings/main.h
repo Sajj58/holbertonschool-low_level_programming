2e34856e0af49d75fcb31ba991326b04c6315f8a
root <kebdanisajjad271199@gmail.com>
2022-06-29 05:16:38 -0700

    task0

--- debugging/pointers_arrays_strings/main.h
+++ debugging/pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
@@ -7,3 +7,4 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+#endif /* MAIN_H */
