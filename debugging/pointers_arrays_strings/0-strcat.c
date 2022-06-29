b2d1be4adc5efd3cee9af9ee221d3843a8dcecc2
root <kebdanisajjad271199@gmail.com>
2022-06-29 05:33:03 -0700

    task0

--- debugging/pointers_arrays_strings/0-strcat.c
+++ debugging/pointers_arrays_strings/0-strcat.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * _strncat - appends src to the dest string
+ * _strcat - appends src to the dest string
  * @dest: string to append by src
  * @src: string to append to dest
  * @n: largest number of bytes to append
  * Return: address of dest
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int a, b;
 
