2257d44d3d92f2d009556d25fa55035f76c06aa3
root <kebdanisajjad271199@gmail.com>
2022-07-01 02:14:24 -0700

    task3

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/*
+print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+{
+	int w;
+	int h;
+	h = 0;
+printf("function that returns a pointer to a 2 dimensional array of intege,%d w,h)
+	
