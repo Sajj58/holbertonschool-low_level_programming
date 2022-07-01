ed41a090a5e784b215fcaf516b42f25adbf2f845
root <kebdanisajjad271199@gmail.com>
2022-07-01 06:41:00 -0700

    task3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -15,9 +15,9 @@ int **alloc_grid(int width, int height)
 
 {
 	int i;
-	int p;
+	int **p;
 
-p =(malloc sizeof 48, 32(int))
-p=(malloc sizeof 128, 128(int))
-Retup = (NULL);
+p =(malloc sizeof 48, 32(int));
+p=(malloc sizeof 128, 128(int));
+Return;
 
