beefde3827b306d58e8edcb3c8035781b43aa306
root <kebdanisajjad271199@gmail.com>
2022-07-04 06:28:58 -0700

    task

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer, pointer fct
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
