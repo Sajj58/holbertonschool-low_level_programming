2101483e892b4d7cf610a94ee51b3299f9d7a8bb
root <kebdanisajjad271199@gmail.com>
2022-07-12 02:07:40 -0700

    task

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include "variadict_fuctions.h"
+#include <stdarg.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+int sum_them_all(const unsigned int n, ...)
 
 {
-	char sum 
+	int i ,sum;
 		sum = 0;
 	printf("%d\n, sam);
 	if n == 0
-	Return = 0 
+		Return = 0 
 }
