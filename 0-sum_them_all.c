994b845962bbb8d45b4b135a196a7e16dbc42f64
root <kebdanisajjad271199@gmail.com>
2022-07-12 01:33:35 -0700

    task

---
+++ 0-sum_them_all.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.c"
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+{
+	int sum;
+	sum = 0;
+	printf("%d\n,sum)
+		Return = 0
+}
