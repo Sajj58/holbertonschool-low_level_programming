151e9abf1414fafb0c4ca1214c69368d2c3dde56
root <kebdanisajjad271199@gmail.com>
2022-07-20 07:57:40 -0700

    taskt

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,3 +1,18 @@
-#!/usr/bin/env bash
-#1
-ps -aux --forest
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements
+ * @h: pointer
+ * Return: number of elements in a linked  list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t link = 0;
+
+	while (h != NULL)
+	{
+		link++;
+		h = h->next;
+	}
+	return (link);
+}
