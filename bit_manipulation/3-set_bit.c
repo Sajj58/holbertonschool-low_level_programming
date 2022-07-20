455a7cd9e1314d9147bf50a418b9ebf883a98118
root <kebdanisajjad271199@gmail.com>
2022-07-20 10:23:15 -0700

    task

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include "main.h"
 
-/*
+/**
  * set_bit - Entry Point
  * @n: input
  * @index: index
  * Return: 0
  */
-
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bits;
